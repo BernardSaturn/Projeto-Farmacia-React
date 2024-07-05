21af2507e37730efb80ae99771e4a88876b49c89
BernardSaturn <beeh.geek@gmail.com>
2024-07-05 13:20:53 -0300

    método do CRUD de Categoria.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
-import "./App.css";
+import './App.css';
 
-import Navbar from "./components/navbar/Navbar";
-import Footer from "./components/footer/Footer";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/home/Home";
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
+import ListaCategorias from './components/listaCategorias/ListaCategorias';
+import FormularioCategoria from './components/categorias/formularioCategoria/FormuarioCategoria';
+import DeletarCategoria from './components/categorias/deletarCategoria/DeletarCategoria';
+
+
 function App() {
   return (
     <>
         <BrowserRouter>
           <Navbar />
-          <div className="min-h-[77.5vh]">
+          <div className='min-h-[77.5vh]'>
             <Routes>
+              <Route path="/" element={<Home />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/categorias" element={<ListaCategorias />} />
+              <Route path="/cadastroCategoria" element={<FormularioCategoria />} />
+              <Route path="/deletarCategoria/:id" element={<DeletarCategoria />} />
+              <Route path="/editarCategoria/:id" element={<FormularioCategoria />} />
             </Routes>
           </div>
           <Footer />
--- src/services/Service.ts
+++ src/services/Service.ts
@@ -4,16 +4,6 @@ const api = axios.create({
     baseURL: 'https://farmaciaapi-utl2.onrender.com/'
 });
 
-export const cadastrarUsuario = async (url: string, dados: any, setDados: Function) => {
-    const resposta = await api.post(url, dados);
-    setDados(resposta.data);
-};
-
-export const login = async (url: string, dados: any, setDados: Function) => {
-    const resposta = await api.post(url, dados);
-    setDados(resposta.data);
-};
-
 export const buscar = async (url: string, setDados: Function) => {
     const resposta = await api.get(url);
     setDados(resposta.data);
